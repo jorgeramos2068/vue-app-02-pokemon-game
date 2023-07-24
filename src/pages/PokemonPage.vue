4929f155e1a89a09c2e83a798efa1a820d09e026
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:25:14 -0600

    Create templates for components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Who is this Pokemon?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+  </div>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+};
+</script>
